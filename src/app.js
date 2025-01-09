3d6f3d2366c0d5fe1058f26df2c157fbc2b93b53
victorshinkevich <supostatekx@gmail.com>
2025-01-09 06:31:32 -0500

    add post /jobs/id/pay

--- src/app.js
+++ src/app.js
@@ -80,6 +80,10 @@ app.get("/jobs/unpaid", getProfile, async (req, res) => {
 
   try {
     const jobs = await Job.findAll({
+      where: {
+        paid: null,
+        // [Op.not]: [{ paid: true }], This is probably some bug.
+      },
       include: [
         {
           attributes: [],
@@ -91,14 +95,101 @@ app.get("/jobs/unpaid", getProfile, async (req, res) => {
           },
         },
       ],
-      where: {
-        paid: false,
-      },
     });
 
     res.json(jobs);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+// POST /jobs/:job_id/pay - Pay for a job, a client can only pay if his balance >= the amount to pay.
+// The amount should be moved from the client's balance to the contractor balance.
+app.post("/jobs/:job_id/pay", getProfile, async (req, res) => {
+  const { Job, Contract, Profile } = req.app.get("models");
+  const { id: profileId, type } = req.profile;
+  const { job_id: jobId } = req.params;
+  if (type !== "client") {
+    return res.status(403).end();
+  }
+
+  const sequelize = req.app.get("sequelize");
+  try {
+    await sequelize.transaction(async (t) => {
+      // Do not check if contract is active - no requirement.
+      const job = await Job.findOne({
+        t,
+        lock: t.LOCK.UPDATE,
+        where: { id: jobId, paid: null },
+      });
+
+      if (!job) {
+        throw new Error("Not found");
+      }
+
+      const { ContractId: contractId } = job;
+      const [client, contractor] = await Promise.all([
+        Profile.findOne({
+          t,
+          lock: t.LOCK.UPDATE,
+          where: { id: profileId },
+          include: [
+            {
+              model: Contract,
+              as: "Client",
+              required: true,
+              where: { id: contractId },
+            },
+          ],
+        }),
+        Profile.findOne({
+          t, // We don't need to lock Contractor while paying him.
+          include: [
+            {
+              model: Contract,
+              as: "Contractor",
+              required: true,
+              where: { id: contractId },
+            },
+          ],
+        }),
+      ]);
+
+      if (!client) {
+        throw new Error("Not allowed");
+      }
+      if (client.balance < job.price) {
+        throw new Error("Bad request");
+      }
+      if (!contractor) {
+        throw new Error();
+      }
+
+      await Promise.all([
+        job.update(
+          {
+            paymentDate: new Date(),
+            paid: true,
+          },
+          t,
+        ),
+        contractor.increment("balance", { by: job.price, t }),
+        client.decrement("balance", { by: job.price, t }),
+      ]);
+    });
+
+    return res.status(200).end();
+  } catch (error) {
+    switch (error.message) {
+      case "Not found":
+        return res.status(404).end();
+      case "Bad request":
+        return res.status(400).end();
+      case "Not allowed":
+        return res.status(403).end();
+      default:
+        return res.status(500).json({ error: error.message });
+    }
   }
 });
 
